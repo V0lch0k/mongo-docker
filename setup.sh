6b5580d83fc315443afe3ef25774cd00434e7db2
root <root@vmSVX-MMT.vpmlh33heyneriey34pxekxbab.px.internal.cloudapp.net>
2018-11-23 05:22:01 +0000

    Webway networks

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --name mongo --restart=always -d -v /opt/mongodb:/data/db --network alpharius -p 27017:27017 mongo
+docker run --name mongo --restart=always -d -v /opt/mongodb:/data/db --network Webway -p 27017:27017 mongo
 
